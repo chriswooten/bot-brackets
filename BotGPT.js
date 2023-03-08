d5d85f20d5cc39017c2b4a893b0df49e9fada427
chriswooten <wootench@gmail.com>
2023-03-08 00:44:24 -0600

    Add RPI code and return

--- BotGPT.js
+++ BotGPT.js
@@ -1,3 +1,4 @@
+// Enter your script here....
 function pickWinner(team1, team2) {
   // Define weights for each stat
   const weights = {
@@ -59,3 +60,114 @@ function pickWinner(team1, team2) {
     return -team2Score / team1Score;
   }
 }
+
+// Some quick utility functions.... 
+
+function Contains(list, item) 
+{ 
+   for (var i = 0; i < list.length; i++) 
+      if (list[i] === item) 
+         return true; 
+   return false; 
+}
+
+function SetProp(obj, prop, val) 
+{ 
+   if (!obj.hasOwnProperty(prop)) 
+      obj[prop] = val; 
+}
+
+function AverageProps(obj) 
+{ 
+   var sum = 0; 
+   var count = 0; 
+   for (var prop in obj) 
+   { 
+      if (obj.hasOwnProperty(prop)) 
+      { 
+         sum += obj[prop]; 
+         count++; 
+      } 
+   } 
+   return count > 0 ? (sum / count) : 0; 
+} 
+
+// Functions used to calculate winning percentages.... 
+// Not extensively tested: may have bugs. Use at your own risk. 
+
+function WinPct(team, excludeTeams) 
+{ 
+   var games = $(team).Games; 
+   var wins = 0; 
+   var losses = 0; 
+   for (var i = 0; i < games.length; i++) 
+   { 
+      if (!Contains(excludeTeams, games[i].Opponent) && $(games[i].Opponent)) 
+      { 
+         wins += games[i].W; 
+         losses += games[i].L; 
+      } 
+   } 
+
+   if (losses == 0) return wins > 0 ? 1 : 0; 
+   else return wins / (wins + losses); 
+}
+
+function OpponentWinPct(team) 
+{ 
+   var hashtable = {}; // to de-dup multiple games against same opponent 
+   var games = $(team).Games; 
+   for (var i = 0; i < games.length; i++) 
+   { 
+      var opp = games[i].Opponent; 
+      if ($(opp) && !hashtable[opp]) 
+         SetProp(hashtable, opp, WinPct(opp, [team])); 
+   } 
+
+   return AverageProps(hashtable); 
+} 
+
+function OpponentOpponentWinPct(team) 
+{ 
+   var hashtable = {}; 
+   var games = $(team).Games; 
+   for (var i = 0; i < games.length; i++) 
+   { 
+      var opp = games[i].Opponent; 
+      var $opp = $(opp); 
+      if ($opp) 
+      { 
+         for (var j = 0; j < $opp.Games.length; j++) 
+         { 
+            var oppopp = $opp.Games[j].Opponent; 
+            var key = "" + opp + "__" + oppopp; 
+            if ($(oppopp) && !hashtable[key]) 
+               hashtable[key] = WinPct(oppopp, [opp, team]); 
+         } 
+      } 
+   } 
+
+   return AverageProps(hashtable); 
+} 
+
+function CalculateRPI(team) 
+{ 
+   return (0.25 * WinPct(team, [])) + 
+      (0.5 * OpponentWinPct(team)) + 
+      (0.25 * OpponentOpponentWinPct(team)); 
+} 
+
+// The actual script.... 
+// Note how we attach the calculation to an expando property so we only have to 
+// calculate it the first time the script runs for a given team. 
+if (!$1.RPI) $1.RPI = CalculateRPI(team1); 
+if (!$2.RPI) $2.RPI = CalculateRPI(team2); 
+print("team1 = " + $1.RPI + ", team2 = " + $2.RPI); // debugging 
+
+// always say never
+if ($2.Seed == 16)
+  return (1);
+else if ($1.Seed == 16)
+  return -1;
+
+return (pickWinner($1, $2));
